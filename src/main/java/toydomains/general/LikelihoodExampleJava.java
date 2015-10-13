d66aaa8828e4e7fcf4ee95f748395faae3767263
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2015-10-13 11:42:44 -0700

    use simplified java api

--- src/main/java/toydomains/general/LikelihoodExampleJava.java
+++ src/main/java/toydomains/general/LikelihoodExampleJava.java
@@ -6,9 +6,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.net.URL;
-import scala.runtime.AbstractFunction2;
 import scala.collection.Seq;
-import scala.reflect.ClassTag;
 import scala.collection.JavaConversions;
 import edu.arizona.sista.odin.*;
 import edu.arizona.sista.processors.Document;
@@ -30,16 +28,12 @@ class LikelihoodExampleJava {
         CoreNLPProcessor proc = new CoreNLPProcessor(true, false, 100);
         // read rules
         String rules = readResource("grammars/likelihood/master.yml");
-        // we need a classtag to create the extractor engine
-        ClassTag<Actions> tag = scala.reflect.ClassTag$.MODULE$.apply(Actions.class);
         // make new extractor engine
-        ExtractorEngine ee = ExtractorEngine.apply(rules, new Actions(), new IdentityAction(), tag);
+        ExtractorEngine ee = ExtractorEngine.apply(rules);
         // annotate text
         Document doc = proc.annotate(text, true);
-        // we need an empty seq for making a new state
-        Seq<Mention> empty = scala.collection.Seq$.MODULE$.empty();
         // extract mentions
-        Seq<Mention> mentions = ee.extractFrom(doc, State.apply(empty));
+        Seq<Mention> mentions = ee.extractFrom(doc);
         // List<Mention> mentionList = JavaConversions.seqAsJavaList(mentions);
         // display mentions
         utils.package$.MODULE$.displayMentions(mentions, doc);
@@ -52,11 +46,4 @@ class LikelihoodExampleJava {
         return new String(bytes, Charset.defaultCharset());
     }
 
-}
-
-// implements the identity action by extending AbstractFunction2
-class IdentityAction extends AbstractFunction2<Seq<Mention>, State, Seq<Mention>> {
-    public Seq<Mention> apply(Seq<Mention> mentions, State state) {
-        return mentions;
-    }
 }
