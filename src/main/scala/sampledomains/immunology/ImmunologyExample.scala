c79373ccc5027744b7116d0b1071294a64c5f9e1
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-11-30 10:01:30 -0700

    Added immunology skeleton

---
+++ src/main/scala/sampledomains/immunology/ImmunologyExample.scala
@@ -0,0 +1,19 @@
+package sampledomains.immunology
+
+import org.clulab.odin.ExtractorEngine
+import org.clulab.processors.fastnlp.FastNLPProcessor
+
+
+object ImmunologyExample extends App {
+  // read rules from general-rules.yml file in resources
+  val source = io.Source.fromURL(getClass.getResource("/grammars/likelihood/master.yml"))
+  val rules = source.mkString
+  source.close()
+
+  // creates an extractor engine using the rules and the default actions
+  val extractor = ExtractorEngine(rules)
+
+  // annotate the sentences
+  val proc = new FastNLPProcessor
+
+}
